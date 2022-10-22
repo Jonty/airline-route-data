4738258dde4a7e76136040847235166b5d3e5a0e
Jonty Wareing <jonty@jonty.co.uk>
2022-10-22 01:26:41 +0100

    Fix scraper

--- scrape_airline_routes.py
+++ scrape_airline_routes.py
@@ -2,74 +2,85 @@
 import sys
 import json
 from collections import defaultdict
+import time
 
-import requests
+import undetected_chromedriver as uc
+from selenium.webdriver.common.by import By
 import lxml.html
 from geopy.distance import geodesic
 
-response = requests.get("https://www.flightsfrom.com/airports")
-airports_json = response.json()
-iatas = [airport["IATA"] for airport in airports_json["response"]["airports"]]
+if __name__ == '__main__':
 
-airports = defaultdict(dict)
+    options = uc.ChromeOptions()
+    options.headless=True
+    options.add_argument('--headless')
+    driver = uc.Chrome(options=options)
 
-while iatas:
-    iata = iatas.pop()
-    if iata in airports:
-        continue
+    driver.get('https://www.flightsfrom.com/airports')
+    response = driver.find_elements(By.TAG_NAME, 'body')[0].text
+    airports_json = json.loads(response)
+    iatas = [airport["IATA"] for airport in airports_json["response"]["airports"]]
 
-    print("Fetching #%s: %s" % (len(airports), iata))
+    airports = defaultdict(dict)
 
-    response = requests.get("https://www.flightsfrom.com/%s/destinations" % iata)
-    
-    root = lxml.html.document_fromstring(response.content)
-    metadata_nodes = root.xpath(
-        '//script[starts-with(., "window.airport=")]'
-    )
-    metadata_tag = metadata_nodes[0].text_content()
-    metadata_bits = metadata_tag.split("window.")
-    
-    metadata = {}
-    for bit in metadata_bits:
-        split = bit.find("=")
-        if split != -1:
-            metadata[bit[:split]] = json.loads(bit[split+1:-1])
+    while iatas:
+        iata = iatas.pop()
+        if iata in airports:
+            continue
 
-    airport_fields = [
-            "city_name",
-            "continent",
-            "country",
-            "country_code",
-            "display_name",
-            "elevation",
-            "IATA",
-            "ICAO",
-            "latitude",
-            "longitude",
-            "name",
-            "timezone",
-    ]
-    airport = {field.lower(): metadata["airport"][field] for field in airport_fields}
-    if airport["elevation"]:
-        airport["elevation"] = int(airport["elevation"])
+        print("Fetching #%s: %s" % (len(airports), iata))
 
-    routes = []
-    for route in metadata["routes"]:
-        orig_ll = (airport["latitude"], airport["longitude"])
-        dest_ll = (route["airport"]["latitude"], route["airport"]["longitude"])
-        distance = int(geodesic(orig_ll, dest_ll).km)
+        driver.get("https://www.flightsfrom.com/%s/destinations" % iata)
+        root = lxml.html.document_fromstring(driver.page_source)
+        metadata_nodes = root.xpath(
+            '//script[contains(., "window.airport")]'
+        )
+        metadata_tag = metadata_nodes[0].text_content()
+        metadata_bits = metadata_tag.split("window.")
+        
+        metadata = {}
+        for bit in metadata_bits:
+            split = bit.find("=")
+            if split != -1:
+                metadata[bit[:split].strip()] = json.loads(bit.strip()[split+2:-1])
 
-        routes.append({
-            "carriers": list(set(a["carrier_name"] for a in route["airlineroutes"])),
-            "km": distance,
-            "min": int(route["common_duration"]),
-            "iata": route["iata_to"],
-        })
+        airport_fields = [
+                "city_name",
+                "continent",
+                "country",
+                "country_code",
+                "display_name",
+                "elevation",
+                "IATA",
+                "ICAO",
+                "latitude",
+                "longitude",
+                "name",
+                "timezone",
+        ]
+        airport = {field.lower(): metadata["airport"][field] for field in airport_fields}
+        if airport["elevation"]:
+            airport["elevation"] = int(airport["elevation"])
 
-        iatas.append(route["iata_to"])
+        routes = []
+        for route in metadata["routes"]:
+            orig_ll = (airport["latitude"], airport["longitude"])
+            dest_ll = (route["airport"]["latitude"], route["airport"]["longitude"])
+            distance = int(geodesic(orig_ll, dest_ll).km)
 
-    airport["routes"] = routes
-    airports[iata] = airport
+            routes.append({
+                "carriers": list(set(a["carrier_name"] for a in route["airlineroutes"])),
+                "km": distance,
+                "min": int(route["common_duration"]),
+                "iata": route["iata_to"],
+            })
 
-with open("airline_routes.json", "w") as f:
-    f.write(json.dumps(airports, indent=4, sort_keys=True, separators=(",", ": ")))
+            iatas.append(route["iata_to"])
+
+        airport["routes"] = routes
+        airports[iata] = airport
+
+        time.sleep(1)
+
+    with open("airline_routes.json", "w") as f:
+        f.write(json.dumps(airports, indent=4, sort_keys=True, separators=(",", ": ")))
